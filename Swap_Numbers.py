1cf564c6aac650009810fe3d7eb018f841c2ca28
varma <psanthoshkumar08@gmail.com>
2018-07-02 19:44:43 -0700

    added swap and area of circle programs

---
+++ Swap_Numbers.py
@@ -0,0 +1,11 @@
+print(" this is Swap numbers program ")
+x=eval(input("Enter x value : "))
+y=eval(input("Etner y value : "))
+
+#swap logic
+x=x+y
+y=x-y
+x=x-y
+#print(x)
+print("After swap y value is : ",x)
+print("After Swap x value is : ",y)
---
+++ area_of_circle.py
@@ -0,0 +1,6 @@
+#area of circle
+Radius=eval(input("enter radius of circle (meters): "))
+#calculating area of circle using math expression a=pi*squre(r)
+pi=3.14
+area=pi*(Radius ** 2)
+print("Area of circle with Radius ",Radius, "is : ",area,"m2")
